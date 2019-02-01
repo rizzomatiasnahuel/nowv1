e9df4d105f392133f8030636b9ccbe7b23243259
Matias Nahuel Rizzo <matiasrizzo@buenosaires.gob.ar>
2019-02-01 18:47:58 -0300

    estrucutra completa completa falta detalles de imagenes

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="StyleSheet" href="css/estilos.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+                <style type="text/css"> 
+
+                        @media (min-width: 576px) {
+                        #recuadro1{
+                           background-image: url("img/i7C.jpg");
+                           background-repeat: no-repeat;
+                           width:100%;
+                           height:450px;
+                           background-position: center;
+                           background-size: cover ;
+                           margin: 0px;
+                           display: block;
+                        } }
+                        div.row{margin-right: 0px; }
+                        div.container-fluid{padding-right: 0px;}
+                    .margen{padding: 0px;}
+                    .border {border-width:1px;border-style:solid;border-color:#000000;}
+                    .back-text{background: #222;color: white;}
+                    .solid {border-style: solid;border: }
+                    .lista{list-style:none;}
+            </style> 
+  </head>
+  <body>
+    <div class="container-fluid ">
+          <div class="row">
+
+              <nav class="navbar navbar-inverse">
+              <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a href="#">
+                    <img  alt="Website Logo" src="img/NOWV2.png"/>
+
+                  </a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                    <li class=""><a href="#">Contacto</a></li>
+                    <li><a href="#">Categorias</a></li>
+                    <li><a href="#">Quienes Somos</a></li>
+                  </ul>
+                  
+                  <ul class="nav navbar-nav navbar-right">
+                  
+                                    <a href="#" class="fa fa-facebook"></a>
+                                    <a href="#" class="fa fa-twitter"></a>
+                                    <a href="#" class="fa fa-google"></a>
+                                    <a href="#" class="fa fa-linkedin"></a>
+                                    <a href="#" class="fa fa-youtube"></a>
+                                    <a href="#" class="fa fa-instagram"></a>
+                  </ul>
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
+            </nav>
+                <div class="row" align="center">
+                <div class="col-xs-12 col-md-12">
+        
+
+                    <h2>
+                      <a href="">
+                       <span> 
+                        Contactanos
+                        <br>
+                        0810 777 0420
+                        </span>
+                        <br>
+                      </a>                
+                      Lunes a Viernes de 10 a 18 hs.
+            
+                    </h2>
+
+
+              </div>
+                
+              </div>
+
+              <div id="carousel-example-generic" class="my-carousel carousel slide" data-ride="carousel">
+              <!-- Indicators -->
+              <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+              </ol>
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                  <img src="img/a2.jpg" alt="...">
+                  <div class="carousel-caption">
+                    <h1 class="titu"> <b> Ofrecemos el mejor servicio </b></h1>
+                    Ofrecemos el mejor servicio
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="img/e4.jpg" alt="...">
+                  <div class="carousel-caption">
+                    <h1 class="titu"> <b>Ofrecemos el mejor servicio</b></h1>
+                    Ofrecemos el mejor servicio
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="img/i5c.jpg" alt="...">
+                  <div class="carousel-caption">
+                    <h1 class="titu"> <b> Ofrecemos el mejor servicio </b></h1>
+                    Ofrecemos el mejor servicio
+                  </div>
+                </div>
+              </div>
+
+              <!-- Controls -->
+              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+
+             <div align="center">
+                <h2> <b> Categorias </b></h2>
+                   
+              </div>
+                    <div class="row">
+                      <div class="col-md-1 border margen" align="center">
+                       <a href=""><img src="img/1.png" class="img-responsive" alt="Responsive image"></a>
+                     </div>
+                      <div class="col-md-1 border margen" align="center">
+                        <a href=""><img src="img/2.png" class="img-responsive" alt="Responsive image"></a>
+                      
+                      </div>
+                      <div class="col-md-1 border margen" align="center">
+                        <a href=""><img src="img/3.png" class="img-responsive" alt="Responsive image"></a>
+
+                      </div>
+                      <div class="col-md-1 border margen" align="center">
+                        <a href=""><img src="img/4.png" class="img-responsive" alt="Responsive image"></a>
+
+                      </div>
+                      <div class="col-md-1 border margen" align="center">
+                        <a href=""><img src="img/5.png" class="img-responsive" alt="Responsive image"></a>
+
+                      </div>
+                      <div class="col-md-1 border margen" align="center">
+                        <a href=""><img src="img/6.png" class="img-responsive" alt="Responsive image"></a>
+
+                      </div>
+                      <div class="col-md-1 border " align="center">
+                        <a href=""><img src="img/7.png" class="img-responsive" alt="Responsive image"></a>
+
+                      </div>
+                      <div class="col-md-1 border margen" align="center">
+                        <a href=""><img src="img/8.png" class="img-responsive" alt="Responsive image"></a>
+
+                      </div>
+                      <div class="col-md-1 border margen" align="center">
+                        <a href=""><img src="img/1.png" class="img-responsive" alt="Responsive image"></a>
+
+                      </div>
+                      <div class="col-md-1 border margen" align="center">
+                        <a href=""><img src="img/1.png" class="img-responsive" alt="Responsive image"></a>
+
+                      </div>
+                      <div class="col-md-1 border margen" align="center">
+                        <a href=""><img src="img/1.png" class="img-responsive" alt="Responsive image"></a>
+
+                      </div>
+                      <div class="col-md-1 border margen" align="center">
+                        <a href=""><img src="img/1.png" class="img-responsive" alt="Responsive image"></a>
+
+                      </div>
+                    </div>
+
+                    <div class="row" id="recuadro1"> 
+                           <div class="row" align="center">
+                          <h2> <b> Quienes Somos? </b></h2>
+                   
+                          </div>
+                                  <p></p>
+                    </div> 
+
+              
+
+                   <div class="row " align="center">
+                            <div class="col-xs-6 col-md-4 solid">
+                                  <ul>
+                                    <li class="lista"><a href="#">Contacto</a></li>
+                                    <li class="lista"><a href="#">Categorias</a></li>
+                                    <li class="lista"><a href="#">Quienes Somos</a></li>
+                                  </ul>
+                            </div>
+                            <div class="col-xs-6 col-md-4 solid">
+                                 <h4>Contactanos</H4>
+                            <h5><a href="">
+                               <span>0810 777 0420</span>
+                              Lunes a Viernes de 10 a 18 hs.
+                                </a>                
+                            </h5>
+                            </div>
+                            <div class="col-xs-12 col-md-4 solid">
+                                  <h4>Seguinos!</H4>
+                                    <ul>
+                                    <a href="#" class="fa fa-facebook"></a>
+                                    <a href="#" class="fa fa-twitter"></a>
+                                    <a href="#" class="fa fa-google"></a>
+                                    <a href="#" class="fa fa-linkedin"></a>
+                                    <a href="#" class="fa fa-youtube"></a>
+                                    <a href="#" class="fa fa-instagram"></a>
+                                    </ul>
+
+                          </div>
+                   </div>   
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="row back-text" align="center">
+  <div class="col-xs-12 col-md-12">
+    © 2019 - Now Construcciones / Todos los derechos reservados - Términos y Condiciones | Políticas de Privacidad
+  </div>
+  
+</div>
+
+
+
+
+
+    </div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
